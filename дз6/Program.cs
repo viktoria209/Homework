eff4f792e1fa45088363a9aef2905596718a45cf
Виктория <209.viktoria@gmail.com>
2023-01-25 20:37:11 +1000

    first2

--- дз6/Program.cs
+++ дз6/Program.cs
@@ -21,6 +21,18 @@ Console.Write("Количество чисел больше 0: " + result);
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double FindY(double k1, double b1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
 Console.Write("Введите b1: ");
 double b1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k1: ");
@@ -30,6 +42,6 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k1: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(k1, b1, x);
 Console.WriteLine($"Точка пересечения двух прямых: ({x}, {y})");
