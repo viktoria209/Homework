bf772dc34ab557c2c59d7b8c24342f554b2171c4
Виктория <209.viktoria@gmail.com>
2023-01-25 20:28:51 +1000

    first

---
+++ дз6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+int Count(int m)
+{
+    int count = 0;
+    for(int i = 0; i < m; i++)
+    {
+        Console.Write("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int result = Count(m);
+Console.Write("Количество чисел больше 0: " + result);
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых: ({x}, {y})");
